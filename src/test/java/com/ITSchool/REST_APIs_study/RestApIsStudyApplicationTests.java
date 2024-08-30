c6102f1b8845049a4d9ec92cc494fba308cd9cbe
Sabin <sabins.badea@gmail.com>
2024-08-30 12:13:35 +0300

    repo initialization

---
+++ src/test/java/com/ITSchool/REST_APIs_study/RestApIsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITSchool.REST_APIs_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApIsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
